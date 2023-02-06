4ce6f07b4b5c18300f202eace023281a08e3f188
leyden <fusionsoccer05@gmail.com>
2023-02-05 20:29:51 -0600

    adding some route functionality to the server.js

--- server.js
+++ server.js
@@ -1,7 +1,34 @@
 const express = require('express');
-// const { sortData } = require('./sortData');
-// const termData = require('./terms.json');
-
+const fs = reqire('fs');
+const path = require('path');
 const PORT = 3001;
+const data = require('./db/db.json');
 
 const app = express();
+
+// middleware POG
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use(express.static('public'));
+
+// get route for index.html
+app.get('/', (req, res) => {
+    res.sendFile(path.join(__dirname, '/public/index.html'))
+});
+// get route for notes.html
+app.get('/notes', (req, res) => {
+    res.sendFile(path.join(__dirname, '/public/notes.html'))
+});
+
+// route for database request
+app.get('/api/notes', (res, res) => {
+    console.log(`${req.method} has been received and being processed`);
+    fs.readFile(`./db/db.json`, `utf-8`, (err, data) => err ? console.log(err) : res.json)
+})
+
+//---------generates a string of random numbers and letters BELOW-----------
+// module.exports = () =>
+//   Math.floor((1 + Math.random()) * 0x10000)
+//     .toString(16)
+//     .substring(1);
+// --------------------------------------------------------------------
