c8f7e522825b494d93d36e21ae4bb878062baf63
leyden <fusionsoccer05@gmail.com>
2023-02-05 20:56:20 -0600

    added a ton of routes and responses for new note. Proud of myself. More soon 100 emoji

--- server.js
+++ server.js
@@ -3,6 +3,7 @@ const fs = reqire('fs');
 const path = require('path');
 const PORT = 3001;
 const data = require('./db/db.json');
+const uuid = require('./helpers/uuid');
 
 const app = express();
 
@@ -22,9 +23,50 @@ app.get('/notes', (req, res) => {
 
 // route for database request
 app.get('/api/notes', (res, res) => {
-    console.log(`${req.method} has been received and being processed`);
+    console.info(`${req.method} has been received and being processed. bop beep boop.`);
     fs.readFile(`./db/db.json`, `utf-8`, (err, data) => err ? console.log(err) : res.json)
-})
+});
+
+app.post('/api/notes', (req, res) => {
+    console.info(`${req.method} request received. Adding note shortly. beep boop.`)
+
+// now we have to destructure... yay! :/
+    const { title, text } = req.body;
+
+    if (title && text) {
+        // new note that we shall save into our big box of fun!
+        const newNote = {
+            title,
+            text,
+            note_id: uuid(),
+        };
+
+        fs.readFile('./db/db.json', 'utf-8', (err, data) => {
+            if(err) {
+                console.error(err);
+            } else {
+                const parsedNotes = JSON.parse(data);
+
+                parsedNotes.push(newNote);
+
+                fs.writeFile('./db/db.json', JSON.stringify(parsedNotes, null, 4),
+                (writeErr) =>
+                writeErr ? console.error(writeErr) : console.info('Successfully logged note. beep beep boop bop.'));
+            };
+        });
+        
+        const response = {
+            status: 'success',
+            body: newNote,
+        };
+
+        console.log(response);
+        res.status(201).json(response);
+    } else {
+        res.status(500).json('Error. Beep Boop. No new note available');
+    }
+
+});
 
 //---------generates a string of random numbers and letters BELOW-----------
 // module.exports = () =>
